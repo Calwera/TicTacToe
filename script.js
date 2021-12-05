72da0fbddc1a3ff5d633cc8dda4225e26be59400
Calwera <calwera@gmail.com>
2021-12-05 22:24:31 +0100

    added restart module

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const mapField = document.querySelectorAll(".game__map-field");
 const map = document.querySelector(".game__map");
+const player = document.querySelectorAll(".game__score-player");
+const button = document.querySelectorAll(".btn");
 
 let activePlayer = 1;
 const arrWiningCombinations = [
@@ -9,35 +11,37 @@ const arrWiningCombinations = [
   [1, 4, 7],
   [1, 5, 9],
   [2, 5, 8],
-  [3, 5, 8],
   [3, 6, 9],
   [3, 5, 7],
   [4, 5, 6],
   [7, 8, 9],
 ];
 
-mapField.forEach((ele) => {
-  ele.addEventListener("click", function () {
+mapField.forEach((field) => {
+  field.addEventListener("click", function () {
     if (
-      //do poprawki blad z playerem 2
-      !ele.classList.contains(`game__map-player-${activePlayer}`) &&
+      !field.classList.contains(`game__map-player-1`) &&
+      !field.classList.contains(`game__map-player-2`) &&
       activePlayer !== 0
-    )
-      ele.classList.add(`game__map-player-${activePlayer}`);
-    if (checkScore(activePlayer) || activePlayer === 0) {
-      activePlayer = 0;
-    } else {
-      if (activePlayer === 1) {
-        activePlayer = 2;
+    ) {
+      field.classList.add(`game__map-player-${activePlayer}`);
+      if (checkScore(activePlayer) || activePlayer === 0) {
+        activePlayer = 0;
       } else {
-        activePlayer = 1;
+        if (activePlayer === 1) {
+          activePlayer = 2;
+          player.forEach((e) => e.classList.toggle("player-active"));
+        } else {
+          activePlayer = 1;
+          player.forEach((e) => e.classList.toggle("player-active"));
+        }
       }
     }
   });
 });
 
 const checkScore = function (player) {
-  let arrScore = [];
+  let arrScore = []; //temporary array of checked player fields
 
   mapField.forEach((ele, id) => {
     if (ele.classList.contains(`game__map-player-${player}`)) {
@@ -45,33 +49,43 @@ const checkScore = function (player) {
     }
   });
 
-  // let z = arrWiningCombinations.some((ele) => {
-  //   ele.every((element, id, tab) => {
-  //     return element === arrScore[id];
-  //   }) == true;
-  // });
-
-  // console.log(
-  //   arrWiningCombinations.some((ele) => {
-  //     ele.some((element, id, tab) => {
-  //       return element === arrScore[id];
-  //     }) === true;
-  //   })
-  // );
-
-  let d = false;
+  let conditionToWin = false;
 
   arrWiningCombinations.forEach((ele) => {
-    let z;
-    z = ele.every((element, id, tab) => {
-      return element === arrScore[id];
-    });
-    if (z === true) {
+    //checking if player has won
+    if (
+      ele.every((element, id, tab) => {
+        if (arrScore.length > 3) {
+          return (
+            element === arrScore[id + 1] ||
+            element === arrScore[id] ||
+            element === arrScore[id + 2]
+          );
+        }
+        return element === arrScore[id];
+      })
+    ) {
       ele.forEach((field) => {
+        //Checked player won
         mapField[field - 1].classList.add("game__map-won");
       });
-      d = z;
+
+      if (player === 1) button[1].style.display = "block";
+      if (player === 2) button[0].style.display = "block";
+
+      conditionToWin = true;
     }
   });
-  return d;
+  return conditionToWin;
 };
+
+const startState = function () {
+  button.forEach((btn) => (btn.style.display = "none"));
+  activePlayer = 1;
+  mapField.forEach((field) => {
+    field.classList.remove("game__map-player-1");
+    field.classList.remove("game__map-player-2");
+    field.classList.remove("game__map-won");
+  });
+};
+button.forEach((btn) => btn.addEventListener("click", startState));
